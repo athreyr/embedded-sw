9f9e7623583e9bb712391037eeb8e76391851ce9
Athrey Ranjith Krishnanunni <athreyr@gmail.com>
2025-01-23 23:29:16 +0530

    Remove stdio and comment out print_statistics in stats.c

--- course1/src/stats.c
+++ course1/src/stats.c
@@ -22,71 +22,40 @@
  * @date 13 Jan 2025
  *
  */
-
-#include <stdio.h>
 #include "stats.h"
+
 #include "platform.h"
 
 /* Size of the Data Set */
-#define SIZE (40)
-
-// void main()
-// {
-
-//   unsigned char test[SIZE] = {34, 201, 190, 154, 8, 194, 2, 6,
-//                               114, 88, 45, 76, 123, 87, 25, 23,
-//                               200, 122, 150, 90, 92, 87, 177, 244,
-//                               201, 6, 12, 60, 8, 2, 5, 67,
-//                               7, 87, 250, 230, 99, 3, 100, 90};
-
-//   /* Other Variable Declarations Go Here */
-//   unsigned int N = SIZE;
-
-//   /* Statistics and Printing Functions Go Here */
-//   printf("Here is the input array:\n");
-//   print_array(test, N);
-
-//   printf("Here is the reverse sorted array:\n");
-//   sort_array(test, N);
-//   print_array(test, N);
-
-//   printf("Here are the statistics:\n");
-//   print_statistics(test, N);
-// }
+// #define SIZE (40)
 
 /* Add other Implementation File Code Here */
 
-void print_statistics(unsigned char *arr, unsigned int N)
-{
-
+/* void print_statistics(unsigned char *arr, unsigned int N) {
   printf("Minimum is '%u'\n", find_minimum(arr, N));
   printf("Maximum is '%u'\n", find_maximum(arr, N));
   printf("Mean is '%u'\n", find_mean(arr, N));
   printf("Median is '%u'\n", find_median(arr, N));
-}
+} */
 
-void print_array(unsigned char arr[], unsigned int N)
-{
+void print_array(unsigned char arr[], unsigned int N) {
+  // Use PRINTF macro function only if VERBOSE flag is defined
 #ifdef VERBOSE
-  for (int i = 0; i < N; i++)
-  {
+  for (int i = 0; i < N; i++) {
     PRINTF("'%u'\n", arr[i]);
   }
 #endif
 }
 
-unsigned char find_median(unsigned char arr[], unsigned int N)
-{
-
+unsigned char find_median(unsigned char arr[], unsigned int N) {
   sort_array(arr, N);
 
   unsigned char mid = 0;
-  if (N % 2 != 0)
-  {                             // if N is odd
-    mid = arr[(N + 1) / 2 - 1]; // (N+1)/2'th value
-  }
-  else
-  { // N is even
+  if (N % 2 != 0) {
+    // N is odd
+    mid = arr[(N + 1) / 2 - 1];  // (N+1)/2'th value
+  } else {
+    // N is even
     mid = (arr[N / 2 - 1] + arr[N / 2]) / 2;
     // mean of N/2'th and (N/2+1)'th values
   }
@@ -94,52 +63,37 @@ unsigned char find_median(unsigned char arr[], unsigned int N)
   return mid;
 }
 
-unsigned char find_mean(unsigned char arr[], unsigned int N)
-{
-
-  unsigned int sum = arr[0]; // can't use char, will overflow
-  for (int i = 1; i < N; i++)
-  {
+unsigned char find_mean(unsigned char arr[], unsigned int N) {
+  unsigned int sum = arr[0];  // can't use char, will overflow
+  for (int i = 1; i < N; i++) {
     sum += arr[i];
   }
   return (sum / N);
 }
 
-unsigned char find_maximum(unsigned char arr[], unsigned int N)
-{
-
+unsigned char find_maximum(unsigned char arr[], unsigned int N) {
   unsigned char max = arr[0];
-  for (int i = 1; i < N; i++)
-  {
-    if (arr[i] > max)
-    {
+  for (int i = 1; i < N; i++) {
+    if (arr[i] > max) {
       max = arr[i];
     }
   }
   return max;
 }
 
-unsigned char find_minimum(unsigned char arr[], unsigned int N)
-{
-
+unsigned char find_minimum(unsigned char arr[], unsigned int N) {
   unsigned char min = arr[0];
-  for (int i = 1; i < N; i++)
-  {
-    if (arr[i] < min)
-    {
+  for (int i = 1; i < N; i++) {
+    if (arr[i] < min) {
       min = arr[i];
     }
   }
   return min;
 }
 
-void sort_array(unsigned char arr[], unsigned int N)
-{
-
+void sort_array(unsigned char arr[], unsigned int N) {
   // insertion sort algorithm
-  for (int i = 1; i < N; i++)
-  {
-
+  for (int i = 1; i < N; i++) {
     // save element at this position because its position may be overwritten
     unsigned char max = arr[i];
 
@@ -149,13 +103,12 @@ void sort_array(unsigned char arr[], unsigned int N)
     bigger scope (see below) */
 
     // move elements smaller than max that are above it, one step below
-    while (j >= 0 && arr[j] < max)
-    {
+    while (j >= 0 && arr[j] < max) {
       arr[j + 1] = arr[j];
       j--;
     }
 
     // insert max at the position where the previous loop exited
-    arr[j + 1] = max; // j would have already been decremented before loop exit
+    arr[j + 1] = max;  // j would have already been decremented before loop exit
   }
 }
